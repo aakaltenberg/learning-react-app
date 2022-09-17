f02dcedbe893ac61e72980d076bbe4f42a39ab61
aakaltenberg <a.a.kaltenberg@yandex.ru>
2022-09-17 23:50:29 +0300

    forms add employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,14 +8,45 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form'
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name:"Jhon C.", salary:800, increase: false, id:1},
+                {name:"Alex M.", salary:3000, increase: true, id:2},
+                {name:"Carl W.", salary:5000, increase: false, id:3}
+            ]
+        }
+    }
 
-    const data = [
-        {name:"Jhon C.", salary:800, increase: false, id:1},
-        {name:"Alex M.", salary:3000, increase: true, id:2},
-        {name:"Carl W.", salary:5000, increase: false, id:3}
-    ]
+    deleteEmployee = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(el => el.id !== id)
+            }
+        })
+    }
 
+    addEmployee = (name, salary) => {
+        if(name === '' || salary === 0) return;
+        const newEmployee= {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newEmployee];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+
+
+    render(){
     return ( 
         <div className="app">
         <AppInfo />
@@ -23,10 +56,13 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
+        <EmployeesList 
+            data={this.state.data}
+            onDelete={this.deleteEmployee}
+        />
+        <EmployeesAddForm onAdd={this.addEmployee}/>
     </div>
-    );
+    );}
 }
 
 export default App;
